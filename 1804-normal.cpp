80f62e35f353fca37d3e3d8bc0ed030811e9e0db
Aléxis Toigo <alexistoigo@gmail.com>
2024-04-23 20:12:59 -0300

    Update 1804-normal.cpp

--- 1804-normal.cpp
+++ 1804-normal.cpp
@@ -1,6 +1,3 @@
-#pragma GCC optimize("Ofast,02")
-#pragma GCC target("avx,avx2,fma")
-
 #include <bits/stdc++.h>
 
 #define IO std::ios_base::sync_with_stdio(false); std::cin.tie(NULL); std::cout.tie(NULL);
@@ -15,6 +12,7 @@
 //const int INF = 0x3f3f3f3f;
 const ll LINF = 0x3f3f3f3f3f3f3f3fLL;
 using namespace std;
+using namespace std::chrono;
 
 int n;
 
@@ -27,11 +25,13 @@ int main() {
 //    FOUT
 #endif
 
+    auto start = high_resolution_clock::now();
+
     cin >> n;
     int x;
     char op;
 
-    vector<int> buggies(n);
+    vector<ll> buggies(n);
 
     for (int i = 0; i < n; i++) {
         cin >> buggies[i];
@@ -45,6 +45,10 @@ int main() {
         } else buggies[x - 1] = 0;
 
     }
+
+    auto stop = high_resolution_clock::now();
+    cout << "Tempo de execucao: " << duration_cast<milliseconds>(stop - start).count() << " milliseconds" << endl;
+
 
     return 0;
 }
